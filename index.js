0355dd1b69b7218472dc73c03c175d471afd6b51
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-02-24 11:39:03 +0300

    assignment complated

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()* 10) + 1;
+
+const num3 = 16;
+const num4 = 4;
+const mod = 4
+num3 % num4 === mod;
+
+
+
+const max = Math.max(10, 14, 5, 11, 5, 2, 20);
